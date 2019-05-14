6a7ea59aa35e57834ccde9ada9704a26f47252d6
Chieh Nien <Jeffnien123321@gmail.com>
2019-05-14 16:14:09 +0800

    2019/5/14

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <opencv2/aruco.hpp>
 #include <opencv2/aruco/dictionary.hpp>
+#include "pid.h"
 using namespace cv;
 using namespace std;
 // --------------------------------------------------------------------------
@@ -19,10 +20,12 @@ int main(int argc, char *argv[])
 	//calibrate
 	Mat intrinsic;
 	Mat distortionCoeffs;
+	Mat outputMapX;
+	Mat outputMapY;
 
 	//detect marker
-	vector<Mat> rvecs;
-	vector<Mat> tvecs;
+	vector<Vec3d> rvecs;
+	vector<Vec3d> tvecs;
 	const float markerLength = 12.7;
 	vector<int> ids;
 	vector<vector<Point2f>> corners;
@@ -62,6 +65,17 @@ int main(int argc, char *argv[])
 	std::cout << "*                                     *" << std::endl;
 	std::cout << "***************************************" << std::endl;
 
+
+	//PID controls
+	PIDManager my_pids("pid.yaml");
+	cv::Mat error(4, 1, CV_64F);
+	cv::Mat output_v(4, 1, CV_64F);
+	/*output_v.at<double>(0, 0) = 0; //x
+	output_v.at<double>(1, 0) = 0; //y
+	output_v.at<double>(2, 0) = 0; //z
+	output_v.at<double>(3, 0) = 0;    //rotation*/
+
+
 	//VideoCapture cap(1);
 	
 	//Mat Mout;
@@ -71,33 +85,40 @@ int main(int argc, char *argv[])
 	while (1) {
 
 		// Key input
-		int key = cv::waitKey(33);
+		int key = cv::waitKey(15);
 		if (key == 0x1b) break;
 
+		output_v.at<double>(0, 0) = 0; //x
+		output_v.at<double>(1, 0) = 0; //y
+		output_v.at<double>(2, 0) = 0; //z
+		output_v.at<double>(3, 0) = 0; //rotation
+		
 		// Get an image
 		cv::Mat image = ardrone.getImage();
-		
-		cout << "fucking" << endl;
+		//Mat Mout;
+		initUndistortRectifyMap(intrinsic, distortionCoeffs, Mat(), intrinsic, image.size(), CV_32FC1, outputMapX, outputMapY);
+		remap(image, image, outputMapX, outputMapY, INTER_LINEAR);
 
 		cv::aruco::detectMarkers(image, dictionary, corners, ids);
 		cv::aruco::estimatePoseSingleMarkers(corners, markerLength, intrinsic, distortionCoeffs, rvecs, tvecs);
-		cout << "test1" << endl;
 		aruco::drawDetectedMarkers(image, corners, ids);
-		cout << "test2" << endl;
+		//cout << "test2" << endl;
 		//cout << rvecs2[0] << endl;
 		//aruco::drawAxis(out, intrinsic, distortionCoeffs, rvecs2, tvecs2, 0.1);
 		for (int index_marker = 0; index_marker < ids.size(); index_marker++)
 		{
-			cout << "rvecs2[0]" << rvecs[index_marker] << endl;
+			//cout << "rvecs[0]" << rvecs[index_marker] << endl;
 			aruco::drawAxis(image, intrinsic, distortionCoeffs, rvecs[index_marker], tvecs[index_marker], 10);
-			cout << "tvecs2[0]" << tvecs[index_marker] << endl;
-			cout << "tvecs2[1]" << tvecs[index_marker + 1] << endl;
-			cout << "tvecs2[2]" << tvecs[index_marker + 2] << endl;
+			//cout << "tvecs[0]" << tvecs[index_marker] << endl;
+			//cout << "tvecs[1]" << tvecs[index_marker + 1] << endl;
+			//cout << "tvecs[2]" << tvecs[index_marker + 2] << endl;
 
 			cout << index_marker << endl;
 		}
 
-		cout << "fuckyou" << endl;
+		//cout << tvecs[0][2] << endl;
+
+		
 		//cout << tvecs2[0]<<endl;
 		//cout << "fuckyou2" << endl;
 		// Take off / Landing 
@@ -116,6 +137,26 @@ int main(int argc, char *argv[])
 		if (key == 'l') vy = -1.0;
 		if (key == 'q') vz = 1.0;
 		if (key == 'a') vz = -1.0;
+
+		//PID speed adjustment
+		if (tvecs.size()) {
+			error.at<double>(0, 0) = tvecs[0][2] - 100; //x
+			//cout << "1\n";
+			error.at<double>(1, 0) = tvecs[0][0]; //y
+			error.at<double>(2, 0) = tvecs[0][1]; //z
+			error.at<double>(3, 0) = rvecs[0][2];    //rotation
+			//cout << "in tvecs size:" << tvecs.size() << "\n";
+			my_pids.getCommand(error, output_v);
+			cout << "output_v[0]: " << output_v.at<double>(0, 0) << endl;
+			vx = output_v.at<double>(0, 0);
+			vy = 0;// output_v.at<double>(1, 0);
+			vz = 0;//output_v.at<double>(2, 0);
+			vr = 0;// output_v.at<double>(3, 0);
+		}
+		//cout << "tvecs size:" << tvecs.size() << "\n";
+
+		//cout << "fuckyou" << endl;
+		//ardrone.move3D(output_v.at<double>(0, 0)*0.7, output_v.at<double>( 1,0)*0.7, output_v.at<double>(2,0)*0.7, output_v.at<double>(3,0));
 		ardrone.move3D(vx, vy, vz, vr);
 
 		// Change camera
